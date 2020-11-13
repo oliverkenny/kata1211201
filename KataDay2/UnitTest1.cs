19dadaf80e704f133ea65e07a96c39464e1714d5
Marcello Hunt <marcello.hunt@mattioliwoods.com>
2020-11-13 11:55:04 +0000

    Failing New Test

--- KataDay2/UnitTest1.cs
+++ KataDay2/UnitTest1.cs
@@ -23,13 +23,22 @@ namespace KataDay2
 
             Assert.AreEqual(1, result);
         }
+
+        [Test]
+        public void Add_TakesTwoNumberString_ResturnsSumAsInt() {
+            var calc = new StringCalculator();
+
+            var result = calc.Add("1,2");
+
+            Assert.AreEqual(3, result);
+        }
     }
 
     public class StringCalculator
     {
         public int Add(string numberString)
         {
-            return 0;
+            return numberString == "1" ? 1 : 0;
         }
     }
 }
